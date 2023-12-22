4cad099e70d1b1a87e09d83cbbcccbb60092bb68
melih <melih.kurukavak@gmail.com>
2023-12-22 17:30:46 +0300

    new features added

--- components/pinturaTest.tsx
+++ components/pinturaTest.tsx
@@ -21,6 +21,7 @@ import {
   plugin_sticker,
   plugin_redact,
   plugin_retouch,
+  plugin_resize,
 
   // The user interface and plugin locale objects
   locale_en_gb,
@@ -34,6 +35,7 @@ import {
   plugin_sticker_locale_en_gb,
   plugin_redact_locale_en_gb,
   plugin_retouch_locale_en_gb,
+  plugin_resize_locale_en_gb,
 
   // Because we use the annotate plugin we also need
   // to import the markup editor locale and the shape preprocessor
@@ -59,10 +61,12 @@ import {
   getShapeById,
   updateShapeById,
   createRetouchShape,
+  getEditorDefaults,
+  selectionToMask,
 } from "@pqina/pintura";
 
 // Import the editor component from `react-pintura`
-import { PinturaEditor, PinturaComponentEvents } from "@pqina/react-pintura";
+import { PinturaEditor } from "@pqina/react-pintura";
 
 // custom functions needed for custom retouch functionality
 import {
@@ -87,7 +91,8 @@ setPlugins(
   plugin_fill,
   plugin_sticker,
   plugin_redact,
-  plugin_retouch
+  plugin_retouch,
+  plugin_resize
 );
 
 // Create our editor configuration
@@ -191,12 +196,14 @@ const editorConfig = {
     ...plugin_sticker_locale_en_gb,
     ...plugin_redact_locale_en_gb,
     ...plugin_retouch_locale_en_gb,
+    ...plugin_resize_locale_en_gb,
     retouchLabel: "Magic",
   },
 };
 
 function ImageEditor() {
   const editorRef = useRef(null);
+  //For add to Shadow button
   const willRenderShapeControls = (controls, selectedShapeId) => {
     console.log("willRenderShapeControls", selectedShapeId);
     console.log("controls:", controls);
@@ -204,14 +211,14 @@ function ImageEditor() {
     const shadowButton = createNode("Button", "shadow", {
       hideLabel: true,
       label: "Shadow",
-      icon: '<g stroke="currentColor" stroke-width=".125em"><path fill="currentColor" d="M0 0h24v24H0z"/><path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" /><path / d="M18 12a6 6 0 0 1 -6 6" /></g>',
+      icon: '<g stroke="currentColor" stroke-width=".125em"><path stroke="currentColor" fill="currentColor" d="M0 0h24v24H0z"/><path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" /><path / d="M18 12a6 6 0 0 1 -6 6" /></g>',
       onclick: () => console.log("shadow button clicked"),
     });
     // Manipulate or add controls here
     appendNode(shadowButton, buttonGroup);
     //controls[1][3].push(shadowButton);
 
-    return [...controls];
+    return controls;
   };
 
   const retouchWillRenderShapeControls = (controls, activeShapeId) => {
@@ -220,14 +227,13 @@ function ImageEditor() {
 
     // get active shape
     const activeShape = getShapeById(
-      //@ts-ignore
-      editorRef.imageManipulation,
+      editorRef.current.editor.imageManipulation,
       activeShapeId
     );
 
     // Add inpaint buttons
     appendRetouchInpaintButtons(controls, {
-      editorRef,
+      ...editorRef.current.editor,
       activeShape,
       // Update prompt
       onupdate: ({ shapePrompt }) =>
@@ -235,8 +241,7 @@ function ImageEditor() {
           text: shapePrompt,
           onconfirm: (text) => {
             createInpaintShape(
-              editorRef,
-              //@ts-ignore
+              editorRef.current.editor,
               createRetouchShape,
               text,
               //@ts-ignore
@@ -246,8 +251,7 @@ function ImageEditor() {
           },
           onclose: () => {
             // clear selection
-            //@ts-ignore
-            editorRef.imageSelection = [];
+            editorRef.current.editor.imageSelection = [];
           },
           onerror: (err) => {
             // handle error
@@ -256,14 +260,11 @@ function ImageEditor() {
       // Generate more results
       ongenerate: ({ shapePrompt, shapeSelection }) => {
         // clear any selection made
-        //@ts-ignore
-        editorRef.imageSelection = [];
+        editorRef.current.editor.imageSelection = [];
 
         // paint new results based on current data
         createInpaintShape(
-          //@ts-ignore
-          editor,
-          //@ts-ignore
+          editorRef.current.editor,
           createRetouchShape,
           shapePrompt,
           shapeSelection,
@@ -278,11 +279,10 @@ function ImageEditor() {
 
       // receive updated value and update the shape
       onchange: ({ value }) => {
-        //@ts-ignore
-        editor.imageManipulation = updateShapeById(
+        editorRef.current.editor.imageManipulation = updateShapeById(
           // shape list
-          //@ts-ignore
-          editor.imageManipulation,
+
+          editorRef.current.editor.imageManipulation,
 
           // shape to update
           activeShapeId,
@@ -299,11 +299,10 @@ function ImageEditor() {
     appendRetouchInpaintResultNavigation(controls, {
       activeShape,
       onupdate: ({ shapeBackgroundImage }) => {
-        //@ts-ignore
-        editor.imageManipulation = updateShapeById(
+        editorRef.current.editor.imageManipulation = updateShapeById(
           // shape list
-          //@ts-ignore
-          editor.imageManipulation,
+
+          editorRef.current.editor.imageManipulation,
 
           // shape to update
           activeShapeId,
@@ -320,6 +319,193 @@ function ImageEditor() {
     return controls;
   };
 
+  //redact feature inside of magic
+
+  /*  const editorDefaults = getEditorDefaults({
+    imageWriter: {
+      // apply redaction to source image
+      preprocessImageSource: async (src, options, onprogress, state) => {
+        const { dest } = await processDefaultImage(src, {
+          imageRedaction: [...state.redaction],
+        });
+        return dest;
+      },
+
+      // remove redaction from state
+      preprocessImageState: (imageState) => {
+        imageState.redaction = [];
+        return imageState;
+      },
+    },
+  }); */
+
+  let imageScalar;
+  let imageScaled;
+
+  const redact = async (selectionItem) => {
+    // Create mask shape
+    const {
+      rect: maskRect,
+      //@ts-ignore
+      shape: maskShape,
+      canvas: maskCanvas,
+    } = await selectionToMask(
+      [selectionItem],
+      editorRef.current.editor.imageSize,
+      editorRef.current.editor.imageState,
+      {
+        // gets an additional band of pixels around the selection
+        padding: 8,
+
+        // returns a canvas
+        format: "canvas",
+
+        // uses alpha for transparent parts
+        foregroundColor: [0, 0, 0, 0],
+      }
+    );
+
+    // Show mask canvas, for debugging purposes
+    {
+      document.getElementById("maskCanvas")?.remove();
+      maskCanvas.id = "maskCanvas";
+      maskCanvas.style.cssText = `margin:10px;border:1px solid silver;background-color:#ccc;background-image:url("data:image/svg+xml,%3Csvg width='8' height='8' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23E5E5E5'%3E%3Cpath d='M0 0h4v4H0zM4 4h4v4H4z'/%3E%3C/g%3E%3C/svg%3E");height:20vh;`;
+      document.body.append(maskCanvas);
+    }
+
+    // Scrambled scaled image data
+    const { dest: scrambledCanvas } = await processDefaultImage(imageScaled, {
+      imageCropLimitToImage: false,
+      imageWriter: {
+        format: "canvas",
+        mimeType: "image/png",
+      },
+
+      // copy transforms
+      imageFlipX: editorRef.current.editor.imageFlipX,
+      imageFlipY: editorRef.current.editor.imageFlipY,
+      imageRotation: editorRef.current.editor.imageRotation,
+
+      // crop area of mask shape
+      imageCrop: {
+        x: maskRect.x * imageScalar,
+        y: maskRect.y * imageScalar,
+        width: maskRect.width * imageScalar,
+        height: maskRect.height * imageScalar,
+      },
+
+      // redact area of mask shape
+      imageRedaction: [
+        {
+          ...maskShape,
+          x: maskShape.x * imageScalar,
+          y: maskShape.y * imageScalar,
+          width: maskShape.width * imageScalar,
+          height: maskShape.height * imageScalar,
+        },
+      ],
+
+      // customize look of redaction
+      imageScrambler: createDefaultImageScrambler({
+        // higher is more pixels
+        dataSizeScalar: 1,
+
+        // higher is more randomness in pixel offset
+        scrambleAmount: 1,
+
+        // higher is more blurry look instead of pixely look
+        blurAmount: 5,
+      }),
+
+      // uncomment to add red overlay, for debugging purposes
+      /*
+                imageAnnotation: [
+                    {
+                        x: 0,
+                        y: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: [1, 0, 0, 0.25],
+                    },
+                ],
+                */
+    });
+
+    // Show mask canvas, for debugging purposes
+    {
+      document.getElementById("scrambledCanvas")?.remove();
+      //@ts-ignore
+      scrambledCanvas.id = "scrambledCanvas";
+      //@ts-ignore
+      scrambledCanvas.style.cssText = `margin:10px;border:1px solid silver;height:20vh;`;
+      //@ts-ignore
+      document.body.append(scrambledCanvas);
+    }
+
+    // Redacted canvas
+    const backgroundImage = document.createElement("canvas");
+    backgroundImage.width = maskRect.width * imageScalar;
+    backgroundImage.height = maskRect.height * imageScalar;
+    const ctx = backgroundImage.getContext("2d");
+    ctx.drawImage(
+      //@ts-ignore
+      scrambledCanvas,
+      0,
+      0,
+      backgroundImage.width,
+      backgroundImage.height
+    );
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.drawImage(
+      maskCanvas,
+      0,
+      0,
+      backgroundImage.width,
+      backgroundImage.height
+    );
+
+    editorRef.current.editor.imageManipulation = [
+      ...editorRef.current.editor.imageManipulation,
+      {
+        ...maskShape,
+
+        // Use redacted canvas as background
+        backgroundImage,
+
+        // Change selection style
+        selectionStyle: "hook",
+
+        // Can't manipulate shape
+        disableStyle: true,
+        disableFlip: true,
+        disableReorder: true,
+        disableManipulate: true,
+      },
+    ];
+  };
+
+  //onSelectionup
+
+  /* editor.on('selectionup', async (selectionItems) => {
+        const lastSelectionItem =
+            selectionItems[selectionItems.length - 1];
+
+        // is not a "inpaint" selection action
+        if (
+            !lastSelectionItem ||
+            lastSelectionItem.action !== 'redact'
+        )
+            return;
+
+        // do something
+        redact(lastSelectionItem);
+
+        // clear image selection
+        editorRef.current.editor.imageSelection = [];
+    }); */
+
+  // Create scaled down version of source image for faster redacting
+
   return (
     <div className="App" style={{ height: "600px" }}>
       {/* @ts-ignore */}
@@ -331,9 +517,45 @@ function ImageEditor() {
           attachInpaintAction(editorRef.current.editor, createRetouchShape);
           attachCleanAction(editorRef.current.editor, createRetouchShape);
         }}
+        onSelectionup={(selectionItems: any) => {
+          console.log("selectionup:", selectionItems.length);
+          const lastSelectionItem = selectionItems[selectionItems.length - 1];
+          if (!lastSelectionItem || lastSelectionItem.action !== "redact")
+            return;
+          redact(lastSelectionItem);
+          console.log("lastSelectionItem:", lastSelectionItem);
+
+          // clear image selection
+          editorRef.current.editor.imageSelection = [];
+        }}
+        onLoad={async (size: any) => {
+          const MAX_IMAGE_SIZE = 512;
+
+          // scale down image file for scrambling purposes
+          const { dest } = await processDefaultImage(
+            editorRef.current.editor.imageFile,
+            {
+              imageWriter: {
+                targetSize: {
+                  width: MAX_IMAGE_SIZE,
+                },
+              },
+            }
+          );
+          imageScalar = Math.min(1, MAX_IMAGE_SIZE / size.width);
+          imageScaled = dest;
+        }}
         src="image.jpeg"
-        /* @ts-ignore */
+        onProcess={(res: any) => {
+          console.log("Result", res);
+          document
+            .querySelector(".result")
+            .setAttribute("src", URL.createObjectURL(res.dest));
+        }}
         retouchWillRenderShapeControls={retouchWillRenderShapeControls}
+        onSelectshape={(a) => {
+          console.log("selectShape:", a);
+        }}
         // set up
 
         stickers={[
